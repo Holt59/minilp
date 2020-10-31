ac7228f47e8fc8fcfe612e9ad7f02701ce7447d6
Mikaël Capelle <capelle.mikael@gmail.com>
2020-10-31 12:37:24 +0100

    Bring some class in the minilp root.

--- minilp/__init__.py
+++ minilp/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.0.1"
 
 import minilp.problems
 import minilp.results
+from minilp.expr import expr, var, cons  # noqa: F401
 
 problem = minilp.problems.problem
 result = minilp.results.result
